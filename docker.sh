f3afae166a87b929331efd3e1a3adc39c6454d48
tim <tim@keynes.fr>
2019-09-17 19:36:26 +0700

    SH script docker with env var for nginx-proxy

--- docker.sh
+++ docker.sh
@@ -1,1 +1,9 @@
-docker run --name 42E -v $(pwd):/usr/share/nginx/html:ro -d nginx
+docker rm -f 42E
+docker run \
+	--name 42E \
+	-v $(pwd):/usr/share/nginx/html:ro \
+	-p :80 \
+	-e "VIRTUAL_HOST=www.42entrepreneurs.com,42entrepreneurs.com" \
+    -e "LETSENCRYPT_HOST=www.42entrepreneurs.com,42entrepreneurs.com" \
+    -e "LETSENCRYPT_EMAIL=tim@keynes.fr" \
+    -d nginx
