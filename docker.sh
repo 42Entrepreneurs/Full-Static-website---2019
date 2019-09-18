6c1c61807c94869dc0e851031d4639276f8b80a3
tim <tim@keynes.fr>
2019-09-18 15:37:28 +0700

    Staff page

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@ docker run \
 	--name 42E \
 	-v $(pwd):/usr/share/nginx/html:ro \
 	-p :80 \
-	-e "VIRTUAL_HOST=www.42entrepreneurs.com,42entrepreneurs.com" \
-    -e "LETSENCRYPT_HOST=www.42entrepreneurs.com,42entrepreneurs.com" \
-    -e "LETSENCRYPT_EMAIL=tim@keynes.fr" \
+	-e "VIRTUAL_HOST=www.42entrepreneurs.com,42entrepreneurs.com,www.42entrepreneurs.fr,42entrepreneurs.fr" \
+    -e "LETSENCRYPT_HOST=www.42entrepreneurs.com,42entrepreneurs.com,www.42entrepreneurs.fr,42entrepreneurs.fr" \
+    -e "LETSENCRYPT_EMAIL=contact@42entrepreneurs.com" \
     -d nginx
