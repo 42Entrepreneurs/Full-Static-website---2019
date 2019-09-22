a1b2c98d75be76bf013bc8b79c7a34f3b7248a9e
root <root@vps716042.ovh.net>
2019-09-22 10:36:00 +0200

    docker compose

--- docker.sh
+++
@@ -1,9 +0,0 @@
-docker rm -f 42E
-docker run \
-	--name 42E \
-	-v $(pwd):/usr/share/nginx/html:ro \
-	-p :80 \
-	-e "VIRTUAL_HOST=www.42entrepreneurs.com,42entrepreneurs.com,www.42entrepreneurs.fr,42entrepreneurs.fr" \
-    -e "LETSENCRYPT_HOST=www.42entrepreneurs.com,42entrepreneurs.com,www.42entrepreneurs.fr,42entrepreneurs.fr" \
-    -e "LETSENCRYPT_EMAIL=contact@42entrepreneurs.com" \
-    -d nginx
